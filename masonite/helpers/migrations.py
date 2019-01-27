e1e94e915d95c631b1f9213282c532de19661a0b
Joseph Mancuso <idmann509@gmail.com>
2019-01-26 22:46:05 -0500

    added ability to show message when migrations are unmigrated

---
+++ masonite/helpers/migrations.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def has_unmigrated_migrations():
+    from wsgi import container
+
+    migration_directory = ['databases/migrations']
+    for key, value in container.providers.items():
+        if type(key) == str and 'MigrationDirectory' in key:
+            migration_directory.append(value)
+    
+    for directory in migration_directory:
+        try:
+            output = bytes(subprocess.check_output(
+                ['orator', 'migrate:status', '-c',
+                    'config/database.py', '-p', directory]
+            )).decode('utf-8')
+
+            if 'No' in output:
+                return True
+        except Exception:
+            pass
+    
+    return False
